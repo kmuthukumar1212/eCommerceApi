1628b5d1f6ce2cb79a63f477ec04918d7e0824eb
voja <voja@gmail.com>
2020-06-09 09:52:27 +0200

    32

--- Api/Controllers/ProductsController.cs
+++ Api/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,10 +16,12 @@ namespace Api.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly ApiContext context;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public ProductsController(ApiContext context)
+        public ProductsController(ApiContext context, IWebHostEnvironment webHostEnvironment)
         {
             this.context = context;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         // GET /api/products
@@ -64,6 +68,70 @@ namespace Api.Controllers
             if (category == null) return NotFound();
 
             return await context.Products.Where(x => x.CategoryId == category.Id).CountAsync();
+        }
+        
+        // POST /api/products
+        [HttpPost]
+        public async Task<ActionResult<Product>> Create([FromForm] Product product)
+        {
+            string imageName = "noimage.png";
+            if (product.ImageUpload != null)
+            {
+                string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                string filePath = Path.Combine(uploadsDir, imageName);
+                FileStream fs = new FileStream(filePath, FileMode.Create);
+                await product.ImageUpload.CopyToAsync(fs);
+                fs.Close();
+            }
+
+            product.Image = imageName;
+
+            context.Products.Add(product);
+
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+        
+        // PUT /api/products
+        [HttpPut]
+        public async Task<ActionResult<Product>> Update([FromForm] Product product)
+        {
+            if (product.ImageUpload != null)
+            {
+                string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+
+                var currentImage = (from p in context.Products 
+                                    where p.Id == product.Id 
+                                    select p.Image).Single();
+
+                if (!string.Equals(currentImage, "noimage.png"))
+                {
+                    string oldImagePath = Path.Combine(uploadsDir, currentImage);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                string filePath = Path.Combine(uploadsDir, imageName);
+                FileStream fs = new FileStream(filePath, FileMode.Create);
+                await product.ImageUpload.CopyToAsync(fs);
+                fs.Close();
+                product.Image = imageName;
+            }
+
+            context.Entry(product).State = EntityState.Modified;
+            if (product.ImageUpload == null)
+            {
+                context.Entry(product).Property("Image").IsModified = false;
+            }
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
