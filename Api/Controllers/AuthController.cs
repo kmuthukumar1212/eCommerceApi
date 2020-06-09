6b33e6108c60f81cdb72099d518fd44a38e74df3
voja <voja@gmail.com>
2020-06-09 11:47:39 +0200

    40

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        // POST /api/auth
+        [HttpPost]
+        public ActionResult Login([FromBody] Login login)
+        {
+            if (login.Username == "admin" && login.Password == "pass")
+            {
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+        }
+    }
+}
